568b10dcaa47158feb473f4615eb07a8208abe83
Vanessa Ortega <ortegavan@live.com>
2025-08-28 18:12:24 -0300

    chore: instala supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://pjckadlyvkslkgeydoli.supabase.co',
+    supabaseKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqY2thZGx5dmtzbGtnZXlkb2xpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzOTA4MzYsImV4cCI6MjA3MTk2NjgzNn0.M46Jt8-bHS6WO4nhwpsggNFGAbfLOCon_LDnwbeKbaY',
+};
